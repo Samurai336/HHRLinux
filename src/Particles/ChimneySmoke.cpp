ac4a4edd90f2a12419637d614cf6e38d80ce2465
Eric <samurai336@gmail.com>
2014-05-04 18:25:45 -0400

    Whent in circles on the Particle system for Chimney smoke,\n but I fixed scaling so I got that going for me... Which is nice

---
+++ src/Particles/ChimneySmoke.cpp
@@ -0,0 +1,5 @@
+#include "ChimneySmoke.h"
+
+ChimneySmoke::ChimneySmoke()
+{
+}
---
+++ src/Particles/ChimneySmoke.h
@@ -0,0 +1,10 @@
+#ifndef CHIMNEYSMOKE_H
+#define CHIMNEYSMOKE_H
+
+class ChimneySmoke : public ParticleEngine
+{
+public:
+    ChimneySmoke();
+};
+
+#endif // CHIMNEYSMOKE_H
